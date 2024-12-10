c913bbfd08111ff4206160447c2c1bdf998de27d
Jakub Andrasi <116459353+bumkand@users.noreply.github.com>
2024-12-10 21:31:12 +0100

    Update get_next_line.h

--- getnextline/get_next_line.h
+++ getnextline/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jakand <jakand@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jaandras <jaandras@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/19 11:15:27 by jdecorte          #+#    #+#             */
-/*   Updated: 2024/12/08 20:07:49 by jakand           ###   ########.fr       */
+/*   Created: 2024/12/10 16:40:09 by jaandras          #+#    #+#             */
+/*   Updated: 2024/12/10 21:14:56 by jaandras         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1 // Value prepared if it is not define in command line
+#  define BUFFER_SIZE 1
 # endif
 
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdint.h>
 
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *string, int character);
-size_t	ft_strlen(const char *string);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_free(char *buffer, char *line);
 void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_free_join(char *buffer, char *line);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
 
 #endif
