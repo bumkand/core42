edeb13dcc92fd32db95a1ba168c27e71190d1829
bumkand <jakub.andrasi@gmail.com>
2024-12-10 20:54:05 +0100

    refresh

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmravec <mmravec@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jakand <jakand@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 21:43:28 by jakand            #+#    #+#             */
-/*   Updated: 2024/12/09 12:29:34 by mmravec          ###   ########.fr       */
+/*   Updated: 2024/12/10 20:50:18 by jakand           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ char	*next_line(char *buffer)
 	return (line);
 }
 
-char	*take_line(char *buffer)
+char	*ft_take_line(char *buffer)
 {
 	char	*line;
 	int		i;
@@ -62,7 +62,7 @@ char	*take_line(char *buffer)
 	return (line);
 }
 
-char	*get_line(int fd, char *buffer)
+char	*ft_get_line(int fd, char *buffer)
 {
 	char	*line;
 	int		char_read;
@@ -98,16 +98,16 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	buffer = get_line(fd, buffer);
+	buffer = ft_get_line(fd, buffer);
 	if (!buffer)
 		return (NULL);
-	read_line = take_line(buffer);
-	if (!read_line)
-	{
-		free(buffer);
-		buffer = NULL;
-		return (NULL);
-	}
+	read_line = ft_take_line(buffer);
+	// if (!read_line)
+	// {
+	// 	free(buffer);
+	// 	buffer = NULL;
+	// 	return (NULL);
+	// }
 	buffer = next_line(buffer);
 	return (read_line);
 }
@@ -125,11 +125,90 @@ char	*get_next_line(int fd)
 // 		perror("Error opening file");
 // 		return (1);
 // 	}
-// 	while ((line = get_next_line(fd)) && ++i < 5)
+// 	while (1)
 // 	{
+// 		line = get_next_line(fd);
+// 		if (line == NULL)
+// 			break;
 // 		printf("%s", line);
 // 		free(line);
 // 	}
 // 	close(fd);
 // 	return (0);
+// }
+
+// void simulate_read_error(int fd)
+// {
+//     printf("Simulating read error...\n");
+//     close(fd); // Force close the file descriptor
+// }
+
+// int main(void)
+// {
+//     int     fd;
+//     char    *next_line;
+//     int     count;
+
+//     count = 0;
+
+//     printf("Reading using BUFFER_SIZE: %d\n", BUFFER_SIZE);
+//     // Open the test file (make sure read.txt exists and has content)
+//     fd = open("file.txt", O_RDONLY);
+//     if (fd == -1)
+//     {
+//         printf("Error opening file\n");
+//         return (1);
+//     }
+
+//     // Print each line read from the file
+//     while (1)
+//     {
+//         next_line = get_next_line(fd);
+//         if (next_line == NULL)
+//             break;
+//         count++;
+//         printf("[%d]:%s\n", count, next_line); // Show line number and content
+//         free(next_line);
+//     }
+
+//     // Test 1: Simulate read error after a few lines
+//     simulate_read_error(fd);
+//     next_line = get_next_line(fd);  // This should fail due to the forced read error
+//     if (next_line == NULL)
+//     {
+//         printf("Read error handled correctly (NULL returned)\n");
+//     }
+//     else
+//     {
+//         printf("Error: Read error not handled correctly\n");
+//         free(next_line);  // Clean up if somehow allocated
+//     }
+
+//     // Reopen the file and read again to test static buffer reset
+//     fd = open("file.txt", O_RDONLY);
+//     if (fd == -1)
+//     {
+//         printf("Error reopening file\n");
+//         return (1);
+//     }
+
+//     printf("\nRe-reading the file after resetting static buffer:\n");
+//     count = 0;
+//     while (1)
+//     {
+//         next_line = get_next_line(fd);
+//         if (next_line == NULL)
+//             break;
+//         count++;
+//         printf("[%d]:%s\n", count, next_line);
+//         free(next_line);
+//     }
+
+//     // Close the file descriptor
+//     close(fd);
+//     printf("Read using BUFFER_SIZE: %d\n", BUFFER_SIZE);
+//     // If using valgrind, you can include this line to trigger memory checking
+//     // system("valgrind --leak-check=full ./a.out");  // Uncomment if running valgrind
+
+//     return (0);
 // }
